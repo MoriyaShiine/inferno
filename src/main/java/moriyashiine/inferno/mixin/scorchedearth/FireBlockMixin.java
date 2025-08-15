58146e25bc345c9fdae32b3d7897154f87d0679b
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2025-08-15 12:33:18 -0500

    fix grass and podzol still being flammable when scorched earth is disabled

--- src/main/java/moriyashiine/inferno/mixin/scorchedearth/FireBlockMixin.java
+++ src/main/java/moriyashiine/inferno/mixin/scorchedearth/FireBlockMixin.java
@@ -4,6 +4,7 @@
 package moriyashiine.inferno.mixin.scorchedearth;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -19,6 +20,26 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(FireBlock.class)
 public class FireBlockMixin {
+	@ModifyReturnValue(method = "getBurnChance(Lnet/minecraft/block/BlockState;)I", at = @At("RETURN"))
+	private int inferno$scorchedEarthBurn(int original, BlockState state) {
+		if (!ModConfig.scorchedEarth) {
+			if (state.isOf(Blocks.GRASS_BLOCK) || state.isOf(Blocks.PODZOL)) {
+				return 0;
+			}
+		}
+		return original;
+	}
+
+	@ModifyReturnValue(method = "getSpreadChance", at = @At("RETURN"))
+	private int inferno$scorchedEarthSpread(int original, BlockState state) {
+		if (!ModConfig.scorchedEarth) {
+			if (state.isOf(Blocks.GRASS_BLOCK) || state.isOf(Blocks.PODZOL)) {
+				return 0;
+			}
+		}
+		return original;
+	}
+
 	@ModifyExpressionValue(method = "trySpreadingFire", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I", ordinal = 1))
 	private int inferno$scorchedEarth(int original, @Local BlockState state) {
 		if (ModConfig.scorchedEarth) {
